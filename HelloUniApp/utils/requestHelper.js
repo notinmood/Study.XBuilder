bad94e8c400a589bfddeeb098239d6c6eaec5726
xiedali <9727005@qq.com>
2024-12-27 20:03:13 +0800

    给请求的方法设置别名

--- HelloUniApp/utils/requestHelper.js
+++ HelloUniApp/utils/requestHelper.js
@@ -1,8 +1,11 @@
 export class RequestHelper {
-	/**	 * 对网络的请求进行封装。返回Promise对象
+	/**
+	 * 对网络的请求进行封装。返回Promise对象
 	 * @param {String} url 请求的地址
 	 * @param {Object} paramsObject 请求的参数
-	 * @param {Object} optionsObject 请求的配置。配置项包括method,header等	 */	static request = (url, paramsObject={}, optionsObject={}) => { 
+	 * @param {Object} optionsObject 请求的配置。配置项包括method,header等
+	 */
+	static request(url, paramsObject = {}, optionsObject = {}) {
 		return new Promise((resolve, reject) => { //异步封装接口，使用Promise处理异步请求
 			uni.request({ //发送请求
 				url: url, //接收请求的API
@@ -12,17 +15,38 @@ export class RequestHelper {
 					if (res.statusCode !== 200) { //因为200是返回成功的状态码，如果不等于200,则代表获取失败,
 						return uni.showToast({
 							title: "数据获取失败！"
-						})
+						});
 					}
-					resolve(res) //成功,将数据返回
+					resolve(res); //成功,将数据返回
 				},
 				fail: (err) => { //失败操作
-					// uni.showToast({
-					// 	title: "请求接口失败！"
-					// })
-					reject(err)
+					uni.showToast({
+						title: "请求接口失败！"
+					});
+					reject(err);
 				}
 			})
 		})
+	}
+
+	/**
+	 * 封装GET请求
+	 * @param {String} url 请求的地址
+	 * @param {Object} paramsObject 请求的参数
+	 * @param {Object} optionsObject 请求的配置。配置项包括header等(如果内部设置了method，本方法会自动变更为GET)  
+	 */
+	static get(url, paramsObject = {}, optionsObject = {}) {
+		optionsObject.method = 'GET';
+		return this.request(url, paramsObject, optionsObject);
+	}
+	/**
+	 * 封装POST请求
+	 * @param {String} url 请求的地址
+	 * @param {Object} paramsObject 请求的参数
+	 * @param {Object} optionsObject 请求的配置。配置项包括header等(如果内部设置了method，本方法会自动变更为POST)
+	 */
+	static post(url, paramsObject = {}, optionsObject = {}) {
+		optionsObject.method = 'POST';
+		return this.request(url, paramsObject, optionsObject);
 	}
 }
